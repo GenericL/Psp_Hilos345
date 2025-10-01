c89360b92e380c72a3101f68af2aa5c2df8cc573
HusbandoEnjoyer <mvillenarevalo@gmail.com>
2025-10-01 17:51:05 +0200

    Ni idea de porque esta en el gitignore

---
+++ Problemas345Hilos/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.ejercicio3.RestauranteConcurrente;
+
+public class Main {
+    public static void main(String[] args) {
+        RestauranteConcurrente restaurante = new RestauranteConcurrente();
+        restaurante.start();
+    }
+}
